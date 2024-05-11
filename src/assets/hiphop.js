cd5efcde9923b38c08d73b902aced50844887592
Yassine-Knaizia <yknaizia@gmail.com>
2024-05-11 20:20:36 +0100

    test

--- src/assets/hiphop.js
+++ src/assets/hiphop.js
@@ -2,7 +2,6 @@
 import playing from "./imgs/playing.png";
 import pausing from "./imgs/pauseing.png";
 import apes from "./imgs/giphy.gif"
-import song0 from "./music/pub.mp3";
 import song1 from "./music/hiphop-rap/2 4 Family - Stay.mp3";
 import song2 from "./music/hiphop-rap/2Pac - 2 Of Amerikaz Most Wanted (feat. Snoop Dogg).mp3";
 
--- src/components/StreamPlayer.jsx
+++ src/components/StreamPlayer.jsx
@@ -2,47 +2,14 @@ import global from 'global'
 import * as process from "process";
 global.process = process;
 import { useState, useEffect, useRef } from "react";
-import ReactHowler from "react-howler";
-import { pictures,hiphop } from "../assets/hiphop";
 import SimplePeer from 'simple-peer';
-/* global VideoPipe */
 
 
 const StreamPlayer = () => {
-  const [play, setPlay] = useState(true); // Set initial state to true
-  const [muted, setMuted] = useState(true); // New state for muting/unmuting
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [nextIndex, setNextIndex] = useState(1); // Initialize nextIndex to 1
-  const [index, setIndex] = useState(0);
-  const [array, setArray] = useState([])
-
   const [streaming, setStreaming] = useState(false);
-  const [stream, setStream] = useState(null);
   const [peer, setPeer] = useState(null);
 
   const audioRef = useRef(null);
-  const peerRef = useRef(null);
-
-  const image = muted ? pictures[0] : pictures[1]; // Update image based on muted state
-  const music = hiphop;
-
-  const toggleAudio = () => {
-    setMuted((prevMuted) => !prevMuted); // Toggle muted state
-  };
-
-
-  const handleEnd = () => {
-    const randomIndex = Math.floor(Math.random() * music.length);
-    setCurrentIndex(nextIndex); // Move to the next song
-    setNextIndex((prevIndex) => (prevIndex + 1) % music.length); // Calculate the index of the next song
-    setArray((prevArray) => [...prevArray, randomIndex]);
-  };
-
-  useEffect(() => {
-    const randomIndex = Math.floor(Math.random() * music.length);
-    setIndex(randomIndex); 
-    setCurrentIndex(randomIndex);
-  }, []);
 
   useEffect(() => {
     // Function to initialize the peer connection
@@ -64,6 +31,7 @@ const StreamPlayer = () => {
         });
 
         setStreaming(true);
+        console.log('streaming', streaming)
       } catch (error) {
         console.error('Error accessing microphone:', error);
       }
@@ -93,33 +61,9 @@ const StreamPlayer = () => {
       setStreaming(true);
     }
   };
-
-  const song = music[currentIndex] ? ( music[currentIndex] ).title :  "";
   
   return (
-    <div className="home">
-      
-      <div className="player-controls">
-      <div className="player">
-        <ReactHowler
-          key={index}
-          src={music[currentIndex]?.url}
-          playing={play}
-          loop={true} // Set loop to true to continue playing after it finishes
-          mute={muted} // Set mute state
-          html5={true}
-          preload={true}
-          onEnd={handleEnd} // Call handleEnd when the current song ends
-        />
-        <img id="pp-img" onClick={toggleAudio} src={image} alt="Mute/Unmute" />
-
-      </div>  
-        <div className="card">
-          <h3 className="title">{song}</h3>
-        </div>
-      </div>
-        <img className="giphy" src={pictures[2]} alt="apes" />
-      
+    <div className="home">      
         <div>
       <button onClick={toggleStreaming}>
         {streaming ? 'Stop Streaming' : 'Start Streaming'}
